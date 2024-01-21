81eb563a95ac840eb7f021505cfae78930e95040
Johan Swan <jswannieman@gmail.com>
2024-01-21 14:12:09 +1300

    Commit ToDoContext JS Component

---
+++ src/contexts/ToDoContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+export const ToDoContext = createContext({
+  listOfToDos: [
+    {
+      id: 1,
+      toDo: "Initial Message",
+      toDoCompleted: false,
+    },
+  ],
+  toDoAdd: (todo) => {},
+  toDoUpdate: (id, todo) => {},
+  toDoDelete: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useToDos = () => {
+  return useContext(ToDoContext);
+};
+
+export const ToDoProvider = ToDoContext.Provider;
