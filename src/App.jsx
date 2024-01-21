05844b0deeec68cb13dce1a5471c96b76edd795b
Johan Swan <jswannieman@gmail.com>
2024-01-21 20:08:58 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import { ToDoProvider } from "./contexts";
+import ToDoAddForm from "./components/ToDoAddForm";
+import ToDoItem from "./components/ToDoItem";
 
 function App() {
   const [listOfToDos, setListOfToDos] = useState([]);
@@ -15,9 +17,11 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(listOfToDos));
   }, [listOfToDos]);
 
-  const toDoAdd = () => {};
+  const toDoAdd = (todo) => {
+    setListOfToDos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
 
-  const toDoUpdate = () => {};
+  const toDoUpdate = (id, todo) => {};
 
   const toDoDelete = () => {};
 
@@ -30,7 +34,16 @@ function App() {
       <div className="bg-[#1a2627] min-h-screen select-none">
         <div className="w-full max-w-5xl text-white mx-auto shadow-lg shadow-white rounded-2xl px-12 py-16">
           <h1 className="text-center mb-6 text-2xl font-bold">My ToDo List</h1>
-          <div className="mb-8"></div>
+          <div className="mb-5">
+            <ToDoAddForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {listOfToDos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <ToDoItem todo={todo} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </ToDoProvider>
